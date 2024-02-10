0014c5d0b89ee2d0de1172282908824f371963cd
Gabriel Larson <gabrieljosephlarson@gmail.com>
2024-02-09 22:52:40 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using glarson_mission_5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace glarson_mission_5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
